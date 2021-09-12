7d18065e9690a57e22a22838528b6a359d864c95
ptaszynski <ptaszynski@users.noreply.github.com>
2021-09-12 23:33:12 +0900

    Add files via upload

---
+++ tfidf_calc_mc.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+# this is a simple tf*idf calculator.
+# it calculates tf*idf for all words in all provided files
+# it assumes that the sentences are already tokenized (word-segmented)
+# all files should have extension .txt just in case
+# use example:
+# > perl tfidf_calc.pl *.txt (to process all txt files in the folder)
+# Written by: Michal Ptaszynski
+# All comments and requests to: ptaszynski@ieee.org
+
+use utf8::all;
+use MCE::Loop chunk_size => 1;#, max_workers => MCE::Util::get_ncpu;
+
+# # TO SHOW HOW MANY ARGUMENTS WERE PROVIDED
+# my $numArgs = $#ARGV + 1;
+# print "thanks, you gave me $numArgs command-line arguments:\n";
+
+my @filenames;
+
+# find all filenames from arguments
+foreach my $argnum (0 .. $#ARGV) {
+    # print "$ARGV[$argnum]\n";
+    my $filename = substr($ARGV[$argnum], 0, index($ARGV[$argnum], '.'));
+    # print "$filename\n";
+    push @filenames, $filename;
+
+    open(FILE_1, "<$filename.txt") or die "Cannot open $file: $!";
+    my @file = <FILE_1>;
+    close FILE_1;
+    my $clusterfuck = join ' ', @file;
+
+    # THIS CONTAINS ALL WORDS IN DOCUMENT
+    @$filename = split ' ', $clusterfuck;
+
+    # THIS CONTAINS ALL UNIQUE WORDS IN DOCUMENT STORED IN PREMAPPED HASH
+    $$filename = { map { $_ => 1 } @$filename };
+
+    # making hash with wordcounts
+    foreach my $wordy (@$filename){
+      $uniqwords{$filename}{$wordy}++;
+    }
+    next;
+}
+
+# foreach my $file (@filenames) {
+mce_loop {
+  my $file = $_;
+  my %tfidfhash;
+
+  # my %tfidfhash = mce_loop {
+  #   my $word = $_;
+  foreach my $word (keys %{$uniqwords{$file}}){
+    my $tf = $uniqwords{$file}{$word}/@$file;
+
+    my $found=0;
+    foreach $tempfilename (@filenames) {
+      # USES TRICK (PREMAPPED HASH) FROM LINE 36
+      $found++ if $$tempfilename->{ $word };
+    }
+
+    my $idf = log(@filenames/$found);
+    # print "IDF for word \"$word\" in file \"$file\" is $idf\n";
+
+    my $tfidf = $tf*$idf;
+    # print "TF-IDF for word \"$word\" in file \"$file\" is $tfidf\n\n";
+
+    $tfidfhash{$word}=$tfidf;
+    # $hashtemp{$word}=$tfidf;
+    # MCE->gather(%hashtemp);
+  }
+  # } keys %{$uniqwords{$file}};
+
+  open(FH, '>', "tfidf_$file.txt") or die $!;
+
+  print FH "$_,$tfidfhash{$_}\n" for sort { $tfidfhash{$b} <=> $tfidfhash{$a} or $a cmp $b } keys %tfidfhash;
+  close(FH);
+} @filenames;
