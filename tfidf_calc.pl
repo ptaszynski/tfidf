474eeca031d6d633ded621c118deb2e3f7010142
ptaszynski <ptaszynski@users.noreply.github.com>
2021-09-13 01:31:44 +0900

    Add files via upload

--- tfidf_calc.pl
+++ tfidf_calc.pl
@@ -1,26 +1,30 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -s
 # this is a simple tf*idf calculator.
 # it calculates tf*idf for all words in all provided files
 # it assumes that the sentences are already tokenized (word-segmented)
 # all files should have extension .txt just in case
 # use example:
 # > perl tfidf_calc.pl *.txt (to process all txt files in the folder)
+# > perl tfidf_calc.pl -bm25 *.txt (if you want to use bm25 weighting)
+# > perl tfidf_calc.pl -bm25plus *.txt (bm25+ seems to improve weighting for very long documents)
 # Written by: Michal Ptaszynski
 # All comments and requests to: ptaszynski@ieee.org
 
 use utf8::all;
+use List::Util qw(sum);
 
 # # TO SHOW HOW MANY ARGUMENTS WERE PROVIDED
 # my $numArgs = $#ARGV + 1;
 # print "thanks, you gave me $numArgs command-line arguments:\n";
 
 my @filenames;
+my @doclengths;
 
 # find all filenames from arguments
 foreach my $argnum (0 .. $#ARGV) {
     # print "$ARGV[$argnum]\n";
     my $filename = substr($ARGV[$argnum], 0, index($ARGV[$argnum], '.'));
-    # print "$filename\n";
+    # print "filename is $filename\n";
     push @filenames, $filename;
 
     open(FILE_1, "<$filename.txt") or die "Cannot open $file: $!";
@@ -30,6 +34,12 @@ foreach my $argnum (0 .. $#ARGV) {
 
     # THIS CONTAINS ALL WORDS IN DOCUMENT
     @$filename = split ' ', $clusterfuck;
+
+    if ($bm25==1 or $bm25plus==1){
+      my $doclength = @$filename;
+      push @doclengths, $doclength;
+      # print "doclength is $doclength\n";
+    }
 
     # THIS CONTAINS ALL UNIQUE WORDS IN DOCUMENT STORED IN PREMAPPED HASH
     $$filename = { map { $_ => 1 } @$filename };
@@ -41,10 +51,28 @@ foreach my $argnum (0 .. $#ARGV) {
     next;
 }
 
+
+if ($bm25==1 or $bm25plus==1){
+  $avglength = sum(@doclengths)/@doclengths;
+  # print "average is $avglength\n";
+  $k = 2;
+  $b = 0.75;
+}
+
+
 foreach my $file (@filenames) {
   my %tfidfhash;
+
   foreach my $word (keys %{$uniqwords{$file}}){
     my $tf = $uniqwords{$file}{$word}/@$file;
+
+    if ($bm25==1){
+      $tf = $uniqwords{$file}{$word}*($k+1)/($uniqwords{$file}{$word}+$k*((1-$b)+$b*(@$file/$avglength)));
+    } elsif ($bm25plus==1){
+      $d = 1;
+      $tf = ($uniqwords{$file}{$word}*($k+1)/($uniqwords{$file}{$word}+$k*((1-$b)+$b*(@$file/$avglength)))+$d);
+    }
+
     my $found=0;
     foreach $tempfilename (@filenames) {
       # USES TRICK (PREMAPPED HASH) FROM LINE 37
@@ -52,7 +80,11 @@ foreach my $file (@filenames) {
     }
 
     my $idf = log(@filenames/$found);
-    # # print "IDF for word \"$word\" in file \"$file\" is $idf\n";
+    # print "IDF for word \"$word\" in file \"$file\" is $idf\n";
+
+    if ($bm25==1 or $bm25plus==1){
+      $idf = log((@filenames-$found+0.5)/($found+0.5));
+    }
 
     my $tfidf = $tf*$idf;
     # print "TF-IDF for word \"$word\" in file \"$file\" is $tfidf\n\n";
